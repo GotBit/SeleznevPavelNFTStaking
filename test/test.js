63828e511718150abe619b4a63ddeff7ac7b891f
Seleznev Pavel <seleznev.pavlex@gmail.com>
2022-01-26 16:48:38 +0300

    test refactoring

--- test/test.js
+++ test/test.js
@@ -1,64 +1,54 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it ("deploy sale and staking", async function() {
-    const Token = await ethers.getContractFactory("Token");
-    const token = await Token.deploy();
+describe("Staking", function () {
+
+  let Token, token, NFT, nft, Sale, sale, Stake, stake, owner, receiver;
+  const tokenId = 12345;
+
+
+  beforeEach(async () => {
+    [owner, receiver] = await ethers.getSigners();
+    Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy();
     await token.deployed();
-    const NFT = await ethers.getContractFactory("NFT");
-    const nft = await NFT.deploy();
+    NFT = await ethers.getContractFactory("NFT");
+    nft = await NFT.deploy();
     await nft.deployed();
-    const Sale = await ethers.getContractFactory("Sale");
-    const sale = await Sale.deploy(token.address, nft.address);
+    Sale = await ethers.getContractFactory("Sale");
+    sale = await Sale.deploy(token.address, nft.address);
     await sale.deployed();
 
-    const Stake = await ethers.getContractFactory("Stake");
-    const stake = await Stake.deploy(token.address, nft.address);
+    Stake = await ethers.getContractFactory("Stake");
+    stake = await Stake.deploy(token.address, nft.address);
+
     await stake.deployed();
 
+    await nft.setSaleAddress(sale.address);
+    await token.setStakeAddress(stake.address);
+  })
+  it ("deploy token and nft", async function() {
+    expect(await token.name()).to.equal("token");
+    expect(await nft.name()).to.equal("non fungible token");
+  });
+  it ("deploy sale and staking", async function() {
     expect(await sale.tokenAddress()).to.equal(token.address);
     expect(await sale.nftAddress()).to.equal(nft.address);
     expect(await stake.tokenAddress()).to.equal(token.address);
     expect(await stake.nftAddress()).to.equal(nft.address);
   });
   it("sale and staking test", async function () {
-    let [owner, receiver] = await ethers.getSigners();
-
-    const Token = await ethers.getContractFactory("Token");
-    const token = await Token.deploy();
-    await token.deployed();
-    expect(await token.name()).to.equal("token");
-    const NFT = await ethers.getContractFactory("NFT");
-    const nft = await NFT.deploy();
-    await nft.deployed();
-    expect(await nft.name()).to.equal("non fungible token");
-    expect(await token.balanceOf(owner.address)).to.equal(1000);
-
-    const Sale = await ethers.getContractFactory("Sale");
-    const sale = await Sale.deploy(token.address, nft.address);
-    await sale.deployed();
-
-    const tokenId = 12345;
     // buy nft
-    await nft.setSaleAddress(sale.address);
+    expect(await token.balanceOf(owner.address)).to.equal(1000);
     expect(await nft.balanceOf(owner.address)).to.equal(0);
     await token.increaseAllowance(sale.address, 10);
     await sale.buy(receiver.address, tokenId);
     expect(await token.balanceOf(owner.address)).to.equal(990);
     expect(await token.balanceOf(receiver.address)).to.equal(10);
-    
     expect(await nft.balanceOf(owner.address)).to.equal(1);
 
 
     // staking
-    const Stake = await ethers.getContractFactory("Stake");
-    const stake = await Stake.deploy(token.address, nft.address);
-    await stake.deployed();
-
-
-    await token.setStakeAddress(stake.address);
-
     
     await nft.approve(stake.address, tokenId);
     await stake.stake(tokenId);
@@ -78,6 +68,5 @@ describe("Greeter", function () {
     await expect(token.mintStakeReward(owner.address, 9999)).to.be.revertedWith("sender address is not stake contract address");
     await expect(nft.mint(owner.address, 9999)).to.be.revertedWith("sender address is not sale contract address");
     
-
   });
 });
